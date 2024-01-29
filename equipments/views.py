2d7ce2c19e9e69b63508b5d8c88b3e9d9e315dc0
lucas richards dev <lucasrichardsdev@gmail.com>
2024-01-29 10:08:19 -0800

    allowed host

--- equipments/views.py
+++ equipments/views.py
@@ -4,11 +4,28 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(request.POST)
     return render(request, "equipments/index.html")
 
-# create
-def create(request):
-    print(request.POST)
-    return render(request, "equipments/create.html")
 
+
+# def tasks_detail(request, proj_id, task_id):
+#     task = Task.objects.get(id=task_id)
+#     profile = Profile.objects.get(user=request.user)
+#     project = Project.objects.get(id=proj_id)
+#     comment_form = CommentForm()
+#     comments = Comment.objects.filter(task=task_id)
     
+#     cannot_edit_task = not (profile.is_manager()
+#                             or task.is_assignee(request.user))
+#     user = request.user
+#     return render(request, 'tasks/detail.html', {
+#         'project': project,
+#         'profile': profile,
+#         'task': task,
+#         'comments': comments,
+#         'comment_form': comment_form,
+#         'cannot_edit_task': cannot_edit_task,
+#         'user': user
+
+#     })

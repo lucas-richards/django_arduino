e0b5559aadebd94c7ca57b103b73a9c7191e2ca1
lucas richards dev <lucasrichardsdev@gmail.com>
2024-01-29 12:48:15 -0800

    post

--- equipments/urls.py
+++ equipments/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create", views.create, name="create"),
+    path("create_equipment", views.create_equipment, name="create_equipment"),
 
 ]
--- equipments/views.py
+++ equipments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import Equipment, Production
+from .models import Equipment, Production
 
 # Create your views here.
 
@@ -10,7 +10,8 @@ def index(request):
     return render(request, "equipments/index.html")
 
 def create_equipment(request):
-    print(request.POST)
+    data = request.POST
+    print(data)
     return 200
 
 

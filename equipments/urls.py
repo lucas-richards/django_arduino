1bac7945d2e3771d085617d930522a4c9012501d
lucas richards dev <lucasrichardsdev@gmail.com>
2024-02-01 11:44:09 -0800

    dataurl

--- equipments/urls.py
+++ equipments/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("data", views.data, name="data"),
+    path("data/", views.data, name="data"),
     path("<int:equipment_id>", views.detail, name="detail"),
 ]

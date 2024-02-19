4ab8dc7cc7838e9061bd301d222e978b176abdc2
lucas richards dev <lucasrichardsdev@gmail.com>
2024-02-18 17:49:51 -0800

    qr

--- equipments/urls.py
+++ equipments/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("data/", views.data, name="data"),
     path("<int:equipment_id>", views.detail, name="detail"),
+    path("qrcode/", views.qrcode, name="qrcode"),
 
 ]
